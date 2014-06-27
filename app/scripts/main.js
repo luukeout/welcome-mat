bf8d89ef7392e8eb5dc9b804bbc1782a9bb209e3
luke olson <luukeout@gmail.com>
2014-06-27 17:46:35 -0400

    tileContainer and picBox added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,5 @@
-console.log('\'Allo \'Allo!');
+'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1b5m7iasgln_90hcq
+
+&address=2114+Bigelow+Ave&citystatezip=TravelersRest%2C+SC'
+
+225 Kimberly Dr, Travelers Rest, SC
